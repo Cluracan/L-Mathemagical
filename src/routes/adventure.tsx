af9567040ed662348c36df59dbec2c3b451518d7
cluracan1 <cluracan1@gmail.com>
2025-07-23 14:25:43 +0100

    chore: remove unused imnport

--- src/routes/adventure.tsx
+++ src/routes/adventure.tsx
@@ -1,4 +1,4 @@
-import { Box, GlobalStyles } from "@mui/material";
+import { Box } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
 import { Link } from "@tanstack/react-router";
 

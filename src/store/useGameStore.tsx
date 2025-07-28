092b3d729c717cf4ed4a7f73ea3a695295edfefb
cluracan1 <cluracan1@gmail.com>
2025-07-28 15:19:17 +0100

    feat: extend gameStorre to include mutable state required for game

---
+++ src/store/useGameStore.tsx
@@ -0,0 +1,48 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { RoomId } from "../assets/data/RoomId";
+
+type GameStoreState = {
+  playerName: string;
+  modernMode: boolean;
+  currentRoom: RoomId;
+  storyLine: string[];
+  stepCount: number;
+  roomsVisited: Set<RoomId>;
+};
+
+type GameStoreActions = {
+  setPlayerName: (playerName: string) => void;
+  toggleGameMode: () => void;
+  setCurrentRoom: (roomId: RoomId) => void;
+  sendToStoryLine: (storyText: string) => void;
+  increaseStepCount: () => void;
+  addToRoomsVisited: (roomId: RoomId) => void;
+};
+
+type GameStore = GameStoreState & GameStoreActions;
+
+export const useGameStore = create<GameStore>()(
+  persist(
+    (set) => ({
+      playerName: "a",
+      modernMode: false,
+      currentRoom: "grass",
+      storyLine: [],
+      stepCount: 0,
+      roomsVisited: new Set(),
+      toggleGameMode: () => set((state) => ({ modernMode: !state.modernMode })),
+      setPlayerName: (playerName: string) => set({ playerName }),
+      setCurrentRoom: (roomId: RoomId) => set({ currentRoom: roomId }),
+      sendToStoryLine: (storyText: string) =>
+        set((state) => ({ storyLine: [...state.storyLine, storyText] })),
+      increaseStepCount: () =>
+        set((state) => ({ stepCount: state.stepCount + 1 })),
+      addToRoomsVisited: (roomId: RoomId) =>
+        set((state) => ({ roomsVisited: state.roomsVisited.add(roomId) })),
+    }),
+    {
+      name: "l-game-storage",
+    }
+  )
+);

e00d36c1babc7a45358ff52c1eecacc27df8bd1f
cluracan1 <cluracan1@gmail.com>
2025-08-21 08:00:04 +0100

    initial commit

--- src/engine/actions/handleBudge.ts
+++ src/engine/actions/handleBudge.ts
@@ -3,3 +3,7 @@ import type { HandleCommand } from "./dispatchCommand";
 export const handleBudge: HandleCommand = (args) => {
   return args.gameState;
 };
+
+//chest and telephone will be joint triggers from telephone NPC, so moving chest completes the puzzle,
+//perhaps the puzzle isn't complete until chest is moved, actually, so you can always keep ringing
+//until you move the chest, at which point the cable snaps and the line is then dead

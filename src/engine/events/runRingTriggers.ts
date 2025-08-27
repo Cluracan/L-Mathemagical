7f7549653afef248f4ee920db369da3e716a221b
cluracan1 <cluracan1@gmail.com>
2025-08-27 21:49:24 +0100

    chore: extract text to map

--- src/engine/events/runRingTriggers.ts
+++ src/engine/events/runRingTriggers.ts
@@ -1,18 +1,21 @@
 import { produce } from "immer";
 import type { PipelineFunction } from "../actions/dispatchCommand";
 
+const ringMessages = {
+  drop: "As you drop the ring, you sense a great magical power blast through you. You see youself fade back into view.",
+  get: "As you pick up the ring, you sense a great magical power blast through you.  The ring has made you, and everything you are carrying invisible.",
+} as const;
+
 export const runRingTriggers: PipelineFunction = (payload) => {
   const { command, target, gameState } = payload;
-  const { storyLine, itemLocation, currentRoom } = gameState;
+  const { itemLocation, currentRoom } = gameState;
   if (target === "ring") {
     switch (command) {
       case "drop":
         if (itemLocation[target] === "player") {
           const nextGameState = produce(gameState, (draft) => {
             draft.isInvisible = false;
-            draft.storyLine.push(
-              "As you drop the ring, you sense a great magical power blast through you. You see youself fade back into view."
-            );
+            draft.storyLine.push(ringMessages.drop);
             draft.itemLocation[target] = currentRoom;
           });
           return { ...payload, gameState: nextGameState, done: true };
@@ -22,9 +25,7 @@ export const runRingTriggers: PipelineFunction = (payload) => {
         if (itemLocation[target] === currentRoom) {
           const nextGameState = produce(gameState, (draft) => {
             draft.isInvisible = true;
-            draft.storyLine.push(
-              "As you pick up the ring, you sense a great magical power blast through you.  The ring has made you, and everything you are carrying invisible."
-            );
+            draft.storyLine.push(ringMessages.get);
             draft.itemLocation[target] = "player";
           });
           return {

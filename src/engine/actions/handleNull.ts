cbad022e8705b264005ce63fce39f1ef1e459682
cluracan1 <cluracan1@gmail.com>
2025-08-30 08:53:21 +0100

    feat: initial commit of handleNull

--- src/engine/actions/handleNull.ts
+++ src/engine/actions/handleNull.ts
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import type { GameState } from "../gameEngine";
 
 type NullCommandArgs = {
@@ -8,6 +9,23 @@ type NullCommandArgs = {
 
 type HandleNullCommand = (args: NullCommandArgs) => GameState;
 
+const nullFeedback = [
+  "I don't understand...",
+  "That doesn't make sense, I'm afraid...",
+  "Try using two word commands, like 'go north', or 'get cube'",
+  "Hmm?",
+  "I don't follow you",
+  "I can't do that...",
+];
+
 export const handleNull: HandleNullCommand = (args) => {
+  const { gameState } = args;
+  const rngIndex = Math.floor(Math.random() * (nullFeedback.length - 1));
+  const nextGameState = produce(gameState, (draft) => {
+    draft.storyLine.push(nullFeedback[rngIndex]);
+    draft.success = false;
+    draft.feedback = "handleNull";
+  });
+
   return args.gameState;
 };

a4eb14abdf0d9a423aaf47656f993e29ca99d21d
cluracan1 <cluracan1@gmail.com>
2025-09-07 09:27:17 +0100

    chore: thoughts on pig

---
+++ src/engine/puzzles/pig/pigNPC.ts
@@ -0,0 +1,16 @@
+// import type { PuzzleNPC } from "../types";
+
+// export const pigNPC: PuzzleNPC = {
+//   usesDialog: true,
+//   description:{
+//     long:"A large pig is staring at you. It has a piece of paper attached to its collar.",
+//     short:"A large pig is staring at you. It has a piece of paper attached to its collar."
+//     completed:"The large pig eyes you warily.",
+//   }
+// };
+
+/*
+
+pig puzzle - add 'paper' to items and only allow single catch
+
+*/

25386a169292937b72425ff553a0ae1ca36e28ef
cluracan1 <cluracan1@gmail.com>
2025-09-07 09:26:37 +0100

    feat: turtleNPC first commit

---
+++ src/engine/puzzles/turtle/turtleNPC.ts
@@ -0,0 +1,29 @@
+import type { PuzzleNPC } from "../types";
+
+export const turtleNPC: PuzzleNPC = {
+  usesDialog: false,
+  description: {
+    long: "In the centre of the courtyard lies a turtle basking in a shaft of sunlight. He seems to be trying to catch your attention. Would you like to investigate?",
+    short:
+      "The turtle is still trying to catch your attention. Will you investigate?",
+    completed: "The turtle sleeps soundly in the middle of the courtyard.",
+  },
+  triggerPuzzleCommand: "say",
+  acceptPuzzleText: ["yes", "y"],
+  rejectPuzzleText: ["no", "n"],
+  feedback: {
+    puzzleAccept:
+      'As you move towards the turtle, he waves a flipper to attract your attention. "Don\'t approach me directly," he warns. "I\'d like to help you", he says, lowering his voice. "We can meet somewhere provided that it is I who catch up with you. Also," he says, lowering his voice even further, "the Drogos have programmed me to move in a special way. Try moving about a little."',
+    puzzleReject: "The turtle looks at you imploringly.",
+    puzzleIsComplete: "The turtle is sleeping",
+    exitsBlocked: null,
+  },
+  examinableItems: {
+    turtle:
+      "The turtle looks back at you. It seems to have a logo on its shell, but you can't make it out.",
+    logo: "You can't make it out...",
+  },
+  rewardItems: {
+    rusty: "player",
+  },
+};

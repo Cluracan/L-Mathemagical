311c3752cb6df4519e64438da0975fffd73e66fd
cluracan1 <cluracan1@gmail.com>
2025-10-02 14:18:02 +0100

    chore: refactor to Immer

--- src/engine/puzzles/ape/apeReducer.ts
+++ src/engine/puzzles/ape/apeReducer.ts
@@ -16,7 +16,7 @@ const isValidConnection = (userInput: string, currentWord: string) => {
       changedLetterCount++;
     }
   }
-  return changedLetterCount <= 1;
+  return changedLetterCount === 1;
 };
 
 type ApeAction =
@@ -28,40 +28,33 @@ export function apeReducer(state: ApeState, action: ApeAction) {
     case "input":
       const userInput = action.userInput.toLowerCase();
       const currentWord = state.word;
-      let nextFeedback = [...state.feedback, userInput];
-      let nextPuzzleCompleted = state.puzzleCompleted;
-      let nextWord = currentWord;
-      if (userInput.length !== APE_TARGET_WORD.length) {
-        nextFeedback.push(apeFeedback.userInput.wrongLength);
-      } else if (!isValidConnection(userInput, currentWord)) {
-        nextFeedback.push(apeFeedback.userInput.doesNotConnect);
-      } else if (!threeLetterWords.has(userInput)) {
-        nextFeedback.push(apeFeedback.userInput.isNotWord);
-      } else if (userInput === APE_TARGET_WORD) {
-        nextFeedback.push(apeFeedback.userInput.success);
-        nextPuzzleCompleted = true;
-        nextWord = userInput;
-      } else {
-        nextFeedback.push(apeFeedback.userInput.validWord);
-        nextWord = userInput;
-      }
-      return {
-        ...state,
-        puzzleCompleted: nextPuzzleCompleted,
-        feedback: nextFeedback,
-        word: nextWord,
-      };
+      return produce(state, (draft) => {
+        draft.feedback.push(userInput);
+        if (userInput.length !== APE_TARGET_WORD.length) {
+          draft.feedback.push(apeFeedback.userInput.wrongLength);
+        } else if (!isValidConnection(userInput, currentWord)) {
+          draft.feedback.push(apeFeedback.userInput.doesNotConnect);
+        } else if (!threeLetterWords.has(userInput)) {
+          draft.feedback.push(apeFeedback.userInput.isNotWord);
+        } else if (userInput === APE_TARGET_WORD) {
+          draft.feedback.push(apeFeedback.userInput.success);
+          draft.puzzleCompleted = true;
+          draft.word = userInput;
+        } else {
+          draft.feedback.push(apeFeedback.userInput.validWord);
+          draft.word = userInput;
+        }
+      });
     case "showDemo":
       return produce(state, (draft) => {
         draft.feedback.push(...apeFeedback.demo);
         draft.status = "play";
       });
     case "reset":
-      return {
-        ...state,
-        word: APE_INIITAL_WORD,
-        feedback: [...state.feedback, apeFeedback.reset, ...apeFeedback.demo],
-      };
+      return produce(state, (draft) => {
+        draft.word = APE_INIITAL_WORD;
+        draft.feedback = [apeFeedback.reset, ...apeFeedback.demo];
+      });
   }
   return state;
 }

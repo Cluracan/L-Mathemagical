790cc007917f5e71b074ad367d360a969e9328b9
cluracan1 <cluracan1@gmail.com>
2025-10-17 20:00:26 +0100

    feat: guardRoomTriggers initial commit

---
+++ src/engine/events/runGuardRoomTriggers.ts
@@ -0,0 +1,32 @@
+import { produce } from "immer";
+import type { PipelineFunction } from "../pipeline/types";
+import { buildRoomDescription } from "../actions/handleLook";
+import { confiscateItems, sendToJail } from "./runDrogoTriggers";
+
+// Narrative Content
+const guardFeedback = {
+  capture: {
+    onEntry:
+      "But before you can do anything the Drogo Robot Guards overpower you. They search you and carry you off.",
+  },
+};
+
+export const runGuardRoomTriggers: PipelineFunction = (payload) => {
+  const { command, target, gameState } = payload;
+  if (gameState.currentRoom !== "guards") {
+    return payload;
+  }
+  if (!gameState.isInvisible) {
+    return produce(payload, (draft) => {
+      draft.gameState.storyLine.push(
+        ...buildRoomDescription(draft.gameState, "move")
+      );
+      draft.gameState.storyLine.push(guardFeedback.capture.onEntry);
+      confiscateItems(draft);
+      sendToJail(draft);
+    });
+  }
+
+  console.log({ command, target });
+  return payload;
+};

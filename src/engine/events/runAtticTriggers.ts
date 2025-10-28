5ca50a8f3f057a90a268e67fa457a8c954a22f48
Martin Willan <m.willan2523@gmail.com>
2025-10-28 13:03:18 +0000

    feat: jailGuard status includes 'complete'. Responses to say/move/look depend on current status. Non-provoking actions are allowed to flow through pipeline

--- src/engine/events/runAtticTriggers.ts
+++ src/engine/events/runAtticTriggers.ts
@@ -1,27 +1,50 @@
 import { produce } from "immer";
 import type { PipelineFunction } from "../pipeline/types";
 import { DROGO_ID_MAX, DROGO_ID_MIN } from "./runDrogoTriggers";
+import type { Command } from "../dispatchCommand";
+import {
+  buildRoomDescription,
+  toRoomDescriptionArgs,
+} from "../utils/buildRoomDescription";
 
 // Types
 export interface JailGuard {
   id: number;
-  state: "inactive" | "alert" | "present";
-  completed: boolean;
+  status: "inactive" | "alert" | "active" | "complete";
 }
+
+type GetGuardResponse = (
+  command: Command,
+  target: string | null,
+  status: "inactive" | "alert"
+) => { alerted: true; message: string } | { alerted: false };
 
 // Narrative Content
 const jailGuardFeedback = {
-  tryDoor:
-    "The attic door is securely locked, but as you try the handle, you hear sounds outside...",
+  inactive: {
+    move: "The attic door is securely locked, but as you try the handle, you hear sounds outside...",
+    look: "The attic door is securely locked, but as you try the handle, you hear sounds outside...",
+    say: "As you speak, you hear sounds outside the locked door...",
+  },
+
+  alert: {
+    move: "The attic door is locked, but your attempt to open it has alerted a Drogo guard! It unlocks the door and enters the room.",
+    look: "You try the door handle, but in doing so alert a Drogo guard! It unlocks the door and enters the room.",
+    say: "Your noise has alerted a Drogo guard! It unlocks the door and enters the room.",
+  },
   scareAttempt: {
     success: "The guard gives a wail of terror and disappears out of sight.",
-    failure:
-      "The guard swears at you and leaves, locking the door behind itself.",
+    failure: {
+      move: "You try to leave the room, but the guard is blocking the exit! It laughs at you and leaves, locking the door behind itself.",
+      say: "The guard laughts at you and leaves, locking the door behind itself",
+      // default:
+      //   "Before you can do anything, the guard swears at you and leaves, locking the door behind itself.",
+    },
   },
 };
 
 const getJailGuardDescription = (id: number) => {
-  return `A Drogo Robot Guard comes into the room with some food. Emblazoned across its front is the number ${String(id ** 2)}`;
+  return `A Drogo Robot Guard stands in front of you. Emblazoned across its front is the number ${String(id ** 2)}`;
 };
 
 // Helpers
@@ -29,10 +52,21 @@ const spawnJailGuard = (): JailGuard => {
   const id =
     Math.floor(Math.random() * (DROGO_ID_MAX - DROGO_ID_MIN + 1)) +
     DROGO_ID_MIN;
-  return { id, state: "inactive", completed: false };
+  return { id, status: "inactive" };
 };
 
-const willScareJailGuard = (target: string, jailGuard: JailGuard) => {
+const getGuardResponse: GetGuardResponse = (command, target, status) => {
+  if (
+    command === "move" ||
+    (command === "say" && target !== null) ||
+    (command === "look" && target === "door")
+  ) {
+    return { alerted: true, message: jailGuardFeedback[status][command] };
+  }
+  return { alerted: false };
+};
+
+const willScareJailGuard = (target: string | null, jailGuard: JailGuard) => {
   const targetValue = Number(target);
   return !isNaN(targetValue) && targetValue === jailGuard.id;
 };
@@ -43,56 +77,91 @@ export const runAtticTriggers: PipelineFunction = (payload) => {
   if (gameState.currentRoom !== "attic") {
     return payload;
   }
-  if (gameState.jailGuard.completed) {
+  if (gameState.jailGuard.status === "complete") {
     return payload;
   }
 
-  // Guard present
   const jailGuard = payload.gameState.jailGuard;
-  switch (jailGuard.state) {
-    case "present": {
-      return produce(payload, (draft) => {
-        if (
-          command === "say" &&
-          target &&
-          willScareJailGuard(target, jailGuard)
-        ) {
-          draft.gameState.storyLine.push(
-            jailGuardFeedback.scareAttempt.success
-          );
-          draft.gameState.jailGuard.completed = true;
-          draft.gameState.keyLocked.jail = false;
-          draft.done = true;
-        } else {
-          draft.gameState.storyLine.push(
-            jailGuardFeedback.scareAttempt.failure
-          );
-          draft.gameState.jailGuard = spawnJailGuard();
-          draft.done = true;
-        }
-      });
-    }
-
-    case "alert": {
-      return produce(payload, (draft) => {
-        draft.gameState.storyLine.push(getJailGuardDescription(jailGuard.id));
-        draft.gameState.jailGuard.state = "present";
-        draft.done = true;
-      });
-    }
-
+  switch (jailGuard.status) {
     case "inactive": {
-      if (
-        (command === "move" && target === "e") ||
-        (command === "look" && target === "door")
-      ) {
+      const guardResponse = getGuardResponse(command, target, jailGuard.status);
+      if (guardResponse.alerted) {
         return produce(payload, (draft) => {
-          draft.gameState.storyLine.push(jailGuardFeedback.tryDoor);
-          draft.gameState.jailGuard.state = "alert";
+          draft.gameState.storyLine.push(guardResponse.message);
+          draft.gameState.jailGuard.status = "alert";
           draft.done = true;
         });
       }
       break;
+    }
+    case "alert": {
+      const guardResponse = getGuardResponse(command, target, jailGuard.status);
+      if (guardResponse.alerted) {
+        return produce(payload, (draft) => {
+          draft.gameState.storyLine.push(guardResponse.message);
+          draft.gameState.jailGuard.status = "active";
+          const guardDescription = getJailGuardDescription(
+            draft.gameState.jailGuard.id
+          );
+          draft.gameState.storyLine.push(guardDescription);
+          draft.done = true;
+        });
+      }
+      break;
+    }
+    case "active": {
+      switch (command) {
+        case "say": {
+          if (willScareJailGuard(target, jailGuard)) {
+            return produce(payload, (draft) => {
+              draft.gameState.storyLine.push(
+                jailGuardFeedback.scareAttempt.success
+              );
+              draft.gameState.jailGuard.status = "complete";
+              draft.gameState.keyLocked.jail = false;
+              draft.done = true;
+            });
+          } else if (target) {
+            return produce(payload, (draft) => {
+              draft.gameState.storyLine.push(
+                jailGuardFeedback.scareAttempt.failure.say
+              );
+              draft.gameState.jailGuard.status = "inactive";
+              draft.done = true;
+            });
+          }
+          break;
+        }
+        case "move": {
+          return produce(payload, (draft) => {
+            draft.gameState.storyLine.push(
+              jailGuardFeedback.scareAttempt.failure.move
+            );
+            draft.gameState.jailGuard.status = "inactive";
+            draft.done = true;
+          });
+        }
+        case "look": {
+          const jailGuardDescription = getJailGuardDescription(jailGuard.id);
+          if (!target) {
+            return produce(payload, (draft) => {
+              const args = toRoomDescriptionArgs(draft.gameState);
+              const roomDescription = buildRoomDescription(args, "look");
+              draft.gameState.storyLine.push(
+                ...roomDescription,
+                jailGuardDescription
+              );
+              draft.done = true;
+            });
+          } else if (target === "guard") {
+            return produce(payload, (draft) => {
+              draft.gameState.storyLine.push(jailGuardDescription);
+              draft.done = true;
+            });
+          }
+          break;
+        }
+      }
     }
   }
   return payload;

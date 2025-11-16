03b9fe4b6f6338baa8190a803ed69cf248571c50
cluracan1 <cluracan1@gmail.com>
2025-11-16 16:09:08 +0000

    feat: accordion-like structure using collapse (less opinionated)

--- src/features/hints/HintsContent.tsx
+++ src/features/hints/HintsContent.tsx
@@ -1,53 +1,65 @@
-import { Box, Card, CardContent, Collapse, Typography } from "@mui/material";
+import { Box, Button, Collapse } from "@mui/material";
 import note from "./images/note.webp";
+import { useState } from "react";
+import { faqData } from "./data/faqData";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 export const HintsContent = () => {
+  const [expanded, setExpanded] = useState<string | null>(null);
+
   return (
-    <>
-      <Box
-        sx={{
-          height: "100vh",
-          width: "100vw",
-          backgroundColor: "#2b392e",
-        }}
-      >
-        <Box sx={{ height: "50vh" }}>
-          <Card
-            sx={{
-              aspectRatio: 1.47,
-              width: "60vw",
-              p: 4,
-              backgroundImage: `url(${note})`,
-              backgroundSize: "cover",
-              backgroundColor: "transparent",
-              boxShadow: "none",
-              color: "black",
-              "& .MuiTypography-root": {
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 4,
+        aspectRatio: 1.47,
+        width: "60vw",
+        p: 8,
+        backgroundImage: `url(${note})`,
+        backgroundSize: "cover",
+      }}
+    >
+      {faqData.map(({ question, answer }, index) => {
+        const isOpen = expanded === `panel${String(index)}`;
+        return (
+          <Box key={`panel${String(index)}`}>
+            <Button
+              fullWidth
+              size="large"
+              endIcon={isOpen ? <ExpandLess /> : <ExpandMore />}
+              id={`faq-header-${String(index)}`}
+              aria-controls={`faq-panel-${String(index)}`}
+              aria-expanded={isOpen}
+              onClick={() => {
+                setExpanded(isOpen ? null : `panel${String(index)}`);
+              }}
+              sx={{
+                justifyContent: "space-between",
+                color: "black",
+                textTransform: "none",
                 fontFamily: "charm",
                 letterSpacing: 0.5,
-              },
-            }}
-          >
-            <CardContent>
-              <Typography>What&apos;s going on?</Typography>
-              <Collapse in={true}>
-                <Typography>
-                  Somewhere nearby is a magical palace full of puzzles and
-                  strange creatures. To move around and interact with the world,
-                  you type simple commands at the prompt. Your experiences may
-                  be intriguing or puzzling. Talk about them with fellow
-                  adventurers, but do not give anything away to those who may be
-                  following. Somewhere nearby is a magical palace full of
-                  puzzles and strange creatures. To move around and interact
-                  with the world, you type simple commands at the prompt. Your
-                  experiences may be intriguing or puzzling. Talk about them
-                  with fellow adventurers, but do not give anything away to
-                  those who may be following.
-                </Typography>
-              </Collapse>
-            </CardContent>
-          </Card>
-        </Box>
-      </Box>
-    </>
+              }}
+            >
+              {question}
+            </Button>
+            <Collapse in={isOpen}>
+              <Box
+                id={`faq-panel-${String(index)}`}
+                aria-labelledby={`faq-header-${String(index)}`}
+                sx={{
+                  p: 4,
+                  color: "black",
+                  fontFamily: "charm",
+                  letterSpacing: 0.5,
+                }}
+              >
+                {answer}
+              </Box>
+            </Collapse>
+          </Box>
+        );
+      })}
+    </Box>
   );
 };
